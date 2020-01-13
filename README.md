520d5073fbe569842c8127a9aa4a939b2c69d1eb
tr4nc3 <rajats@gmail.com>
2020-01-12 21:30:33 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# SkypeTest
+This is an old archived utility for testing VoIP systems / telephone menus - it can also act as a home-grown robo-dialer. 
+
+# Description
+This is an unmaintained utility intended to send DTMF tones over skype to test Voice-enabled menu systems.  This is simply a test harness written almost 12 years ago ... most likely better alternatives exist now.  
---
+++ skypetest.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+# -------------------------------------------------------------------------------------------
+#  Python / Skype4Py example that takes a skypename from command line parameter,
+#  checks if that skypename is in contact list and if yes then starts a call to that skypename.
+#
+#  Tested with  Skype4Py version 0.9.28.2 and Skype verson 3.5.0.214
+
+import sys
+import time
+import Skype4Py
+
+# This variable will get its actual value in OnCall handler
+CallStatus = 0
+
+# Here we define a set of call statuses that indicate a call has been either aborted or finished
+CallIsFinished = set ([Skype4Py.clsFailed, Skype4Py.clsFinished, Skype4Py.clsMissed, Skype4Py.clsRefused, Skype4Py.clsBusy, Skype4Py.clsCancelled]);
+
+def CreateDTMFTones(dialstr,parSkype,callID):
+    for count in range(0,len(dialstr)):
+        parSkype.SendCommand(parSkype.Command('SET CALL ' + str(callID) + ' DTMF ' + str(dialstr[count])))
+        time.sleep(1)            
+        
+def AttachmentStatusText(status):
+    return skype.Convert.AttachmentStatusToText(status)
+
+def CallStatusText(status):
+    return skype.Convert.CallStatusToText(status)
+
+# This handler is fired when status of Call object has changed
+def OnCall(call, status):
+    global CallStatus
+    CallStatus = status
+    print 'Call status: ' + CallStatusText(status)
+
+# This handler is fired when Skype attatchment status changes
+def OnAttach(status):
+    print 'API attachment status: ' + AttachmentStatusText(status)
+    if status == Skype4Py.apiAttachAvailable:
+        skype.Attach()
+
+# Let's see if we were started with a command line parameter..
+try:
+    CmdLine = sys.argv[1]
+except:
+    print 'Missing command line parameter'
+    sys.exit()
+
+# Creating Skype object and assigning event handlers..
+skype = Skype4Py.Skype()
+skype.OnAttachmentStatus = OnAttach
+skype.OnCallStatus = OnCall
+
+# Starting Skype if it's not running already..
+if not skype.Client.IsRunning:
+        print 'Starting Skype..'
+        skype.Client.Start()
+
+# Attatching to Skype..
+print 'Connecting to Skype..'
+skype.Attach()
+
+#place call
+call = skype.PlaceCall(CmdLine)
+
+CallStatus = call.Status
+#DTMF tones
+while call.Status!=Skype4Py.clsInProgress:
+    continue
+
+if call.Status == Skype4Py.clsInProgress:
+        print 'Waiting to send DTMF'
+        time.sleep(10)
+        skype.SendCommand(skype.Command('SET CALL ' + str(call.Id) + ' DTMF ' + '1'))
+        time.sleep(5)
+        skype.SendCommand(skype.Command('SET CALL ' + str(call.Id) + ' DTMF ' + '1'))
+        time.sleep(7)
+        CreateDTMFTones('1234567890',skype,call.Id)
+        #skype.SendCommand(skype.Command('SET CALL ' + str(call.Id) + ' DTMF ' + '9276'))
+sys.exit()
+
+# Loop until CallStatus gets one of "call terminated" values in OnCall handler
+while not CallStatus in CallIsFinished:
+    pass
+
